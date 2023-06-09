ddab52f2a9a31493855abdbfbd46a9e9039a3f59
Ayush Singh <ayushsingh2442@gmail.com>
2023-06-09 09:58:04 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly object = new RWOnly();
+        // java: name has private access in com.driver.RWOnly
+        object.setName("Ayush");
+
+        System.out.println(object.getName());
+    }
 }
